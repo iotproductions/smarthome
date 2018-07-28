4a9132cf70057c8a5c971cb3baac0bb862ec6751
IoTProductions <lethanhtrieuk36@gmail.com>
2018-07-28 18:08:39 +0700

    Initial commit

---
+++ config/database.js
@@ -0,0 +1,5 @@
+// config/database.js
+module.exports = {
+
+	'url' : 'mongodb://trieule:ttp2018@ds259175.mlab.com:59175/smarthome' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+};
